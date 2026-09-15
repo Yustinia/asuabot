2e8c3e3a83c37fd327fa8b80aa8c9e0aa0cfa009
Yustinia <yustinia@protonmail.com>
2026-09-15 22:17:53 +0800

    cleanup

--- lua/entities/npc_asuabot/helper.lua
+++ lua/entities/npc_asuabot/helper.lua
@@ -228,3 +228,16 @@ function ENT:HandleSpeed(speed, accel)
 	self.loco:SetDesiredSpeed(setSpd)
 	self.loco:SetAcceleration(setAccel)
 end
+
+function ENT:IsObservedBy(ply)
+	if not IsValid(ply) then
+		return false
+	end
+	if not ply:IsLineOfSightClear(self) then
+		return false
+	end
+
+	local dirToBot = (self:GetPos() - ply:GetPos()):GetNormalized()
+	local plyAim = ply:GetAimVector()
+	return plyAim:Dot(dirToBot) > 0.5
+end
--- lua/entities/npc_asuabot/init.lua
+++ lua/entities/npc_asuabot/init.lua
@@ -11,12 +11,10 @@ include("states/state_stealth.lua")
 util.AddNetworkString("Asuabot_ThirdPersonToggle")
 util.AddNetworkString("Asuabot_DisplayJumpscare")
 
--- Listen for third-person toggles and flag the player
 net.Receive("Asuabot_ThirdPersonToggle", function(len, ply)
 	if IsValid(ply) then
 		ply.Asuabot_IsThirdPerson = true
 
-		-- Reset the flag after a short delay so they aren't permanently marked
 		timer.Simple(2, function()
 			if IsValid(ply) then
 				ply.Asuabot_IsThirdPerson = false
@@ -26,33 +24,15 @@ net.Receive("Asuabot_ThirdPersonToggle", function(len, ply)
 end)
 
 function ENT:Initialize()
-	self:SetModel("models/player/kleiner.mdl") -- Invisible physical hull
+	self:SetModel("models/player/kleiner.mdl")
 	self:SetHealth(1000)
 	self:SetCollisionGroup(COLLISION_GROUP_NPC)
 	self:SetSolid(SOLID_BBOX)
 
 	self:SetCollisionBounds(Vector(-16, -16, 0), Vector(16, 16, 72))
 
-	--self:TeleportToDistantNavSpot()
+	self:TeleportToDistantNavSpot()
 	self.CurrentState = "Behind"
-end
-
--- ==========================================
--- Helper Functions
--- ==========================================
-
--- Checks if the Nextbot is inside the player's FOV and not behind a wall
-function ENT:IsObservedBy(ply)
-	if not IsValid(ply) then
-		return false
-	end
-	if not ply:IsLineOfSightClear(self) then
-		return false
-	end
-
-	local dirToBot = (self:GetPos() - ply:GetPos()):GetNormalized()
-	local plyAim = ply:GetAimVector()
-	return plyAim:Dot(dirToBot) > 0.5
 end
 
 -- Nextbot loop
@@ -75,12 +55,10 @@ function ENT:RunBehaviour()
 		elseif self.CurrentState == "FakeOutRush" then
 			self:StateFakeOutRush()
 		else
-			-- Fallback if state is undefined
 			self.CurrentState = "Wander"
 			self:StateWander()
 		end
 
-		-- Safety yield: Prevents infinite loop server crashes if a state function exits immediately
 		coroutine.yield()
 	end
 end
