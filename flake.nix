76ad4baec07bc5bbd513c8a9262baa7fb0ff2938
Yustinia <yustinia@protonmail.com>
2026-09-16 23:44:43 +0800

    integrate glua components

--- flake.nix
+++ flake.nix
@@ -13,6 +13,12 @@
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
+
+      glua-api-snippets = pkgs.fetchzip {
+        url = "https://github.com/luttje/glua-api-snippets/releases/download/2026-08-31_17-51-30/2026-08-31_17-51-30.lua.zip";
+        stripRoot = false;
+        hash = "sha256-YSI0dq57x22v8CYLV0ZJ/IPIN2A158DmtKPb/6WMm1M=";
+      };
     in
     {
       devShells.${system}.default = pkgs.mkShell {
@@ -21,6 +27,10 @@
           stylua # fmt
           lua-language-server # lsp
         ];
+
+        shellHook = ''
+          ln -sfn "${glua-api-snippets}" ./.glua-api
+        '';
       };
     };
 }
