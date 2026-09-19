5e5b59f339e571d3818efa7f8f4b8663bd79b022
Yustinia <yustinia@protonmail.com>
2026-09-19 22:48:12 +0800

    refactor physical lua

--- lua/entities/npc_asuabot/ability/physical.lua
+++ lua/entities/npc_asuabot/ability/physical.lua
@@ -1,25 +1,37 @@
 function ENT:PushEntity(target, force)
 	force = force or 1500
 
-	if CurTime() >= self.NextPushTime then
-		local pushVec = target:GetPos() - self:GetPos()
-		pushVec.z = 15
-		pushVec:Normalize()
-
-		target:SetVelocity(pushVec * force)
-
-		self.NextPushTime = CurTime() + self.PushCD
+	if CurTime() < self.NextPushTime then
+		return
 	end
+
+	if not IsValid(target) then
+		return
+	end
+
+	local pushVec = target:GetPos() - self:GetPos()
+	pushVec.z = 15
+	pushVec:Normalize()
+
+	target:SetVelocity(pushVec * force)
+
+	self.NextPushTime = CurTime() + self.PushCD
 end
 
 function ENT:DamageEntity(target, amount)
 	amount = amount or 1
 
-	if CurTime() >= self.NextDamageTime then
-		target:TakeDamage(amount, self, self)
-
-		self.NextDamageTime = CurTime() + self.DamageCD
+	if CurTime() < self.NextDamageTime then
+		return
 	end
+
+	if not IsValid(target) then
+		return
+	end
+
+	target:TakeDamage(amount, self, self)
+
+	self.NextDamageTime = CurTime() + self.DamageCD
 end
 
 function ENT:PullEntity(target, force)
