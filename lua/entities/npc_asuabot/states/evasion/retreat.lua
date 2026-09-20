729ce0a890a17e7857be8e3cecb07168e6bd46e6
Yustinia <yustinia@protonmail.com>
2026-09-20 19:41:44 +0800

    retreat dist

--- lua/entities/npc_asuabot/states/evasion/retreat.lua
+++ lua/entities/npc_asuabot/states/evasion/retreat.lua
@@ -3,12 +3,13 @@ local RETREAT_ACCEL = 2400
 local RETREAT_GOAL_THRESH = 0
 local RETREAT_PATH_AGE = 0.1
 local RETREAT_AHEAD_DIST = 20
+local RETREAT_DIST = 3000
 
 function ENT:StateRetreat()
 	self:HandleSpeed(RETREAT_SPD, RETREAT_ACCEL)
 	self.Target = self:FindClosestPlayer()
 
-	local targetPos = self:FindClosestHideSpot(3000)
+	local targetPos = self:FindClosestHideSpot(RETREAT_DIST)
 
 	if not self:ComputeRoutingPath(targetPos, RETREAT_AHEAD_DIST, RETREAT_GOAL_THRESH, "Follow") then
 		return
