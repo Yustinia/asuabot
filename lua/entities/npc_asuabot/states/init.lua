c8da303a64e7491886ef200a77637bf80a3aa958
Yustinia <yustinia@protonmail.com>
2026-09-20 10:02:45 +0800

    prep state ambush

--- lua/entities/npc_asuabot/states/init.lua
+++ lua/entities/npc_asuabot/states/init.lua
@@ -13,3 +13,4 @@ include("entities/npc_asuabot/states/evasion/retreat.lua")
 -- Stealth
 include("entities/npc_asuabot/states/stealth/hide.lua")
 include("entities/npc_asuabot/states/stealth/creep.lua")
+include("entities/npc_asuabot/states/stealth/ambush.lua")
