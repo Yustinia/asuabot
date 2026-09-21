1561daf7797d6c3f7a95264c8a44c90be423f935
Yustinia <yustinia@protonmail.com>
2026-09-21 20:18:53 +0800

    file restructure

--- lua/entities/npc_asuabot/cl_init.lua
+++
@@ -1,9 +0,0 @@
-include("shared.lua")
-
--- Using a default Garry's Mod sprite material as a placeholder
-local botMaterial = Material("vgui/entities/npc_asuabot")
-
-function ENT:Draw()
-	render.SetMaterial(botMaterial)
-	render.DrawSprite(self:GetPos() + Vector(0, 0, 50), 100, 100, color_white)
-end
--- lua/entities/npc_asuabot/shared.lua
+++
@@ -1,4 +0,0 @@
-ENT.Base = "base_nextbot"
-ENT.PrintName = "Asuabot"
-ENT.Category = "Nextbot"
-ENT.Spawnable = true
