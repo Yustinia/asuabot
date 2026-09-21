4f1dbf8dde917191cd258a391245a31f537a55ef
Yustinia <yustinia@protonmail.com>
2026-09-21 09:27:15 +0800

    add player health returns

--- lua/entities/npc_asuabot/player/threat.lua
+++ lua/entities/npc_asuabot/player/threat.lua
@@ -8,3 +8,27 @@
 -- function ENT:IsPlayerAbleToDamageBot() end
 -- function ENT:GetPlayerEffectiveAttackRange() end
 -- function ENT:GetPlayerThreatLevel() end
+
+function ENT:GetPlayerHealth()
+	if not IsValid(self.Target) then
+		return
+	end
+
+	return self.Target:Health()
+end
+
+function ENT:IsPlayerHealthy()
+	return self:GetPlayerHealth() >= 65
+end
+
+function ENT:IsPlayerWounded()
+	return self:GetPlayerHealth() < 65
+end
+
+function ENT:IsPlayerCritical()
+	return self:GetPlayerHealth() <= 25
+end
+
+function ENT:IsPlayerNearDeath()
+	return self:GetPlayerHealth() <= 10
+end
