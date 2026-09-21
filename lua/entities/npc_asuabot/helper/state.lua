6a47874aa74ae3cd2925a10971170411097a1580
Yustinia <yustinia@protonmail.com>
2026-09-21 09:27:33 +0800

    add weighted roll helper

--- lua/entities/npc_asuabot/helper/state.lua
+++ lua/entities/npc_asuabot/helper/state.lua
@@ -11,3 +11,20 @@ function ENT:SetState(newState)
 		onEnter(self)
 	end
 end
+
+--- rolls a weighted chance
+--- @param choices table array of { chance = number, value = string }
+--- @return nil
+function ENT:WeightedRoll(choices)
+	local roll = math.random()
+	local cumulative = 0
+
+	for _, choice in ipairs(choices) do
+		cumulative = cumulative + choice.chance
+		if roll < cumulative then
+			return choice.value
+		end
+	end
+
+	return nil
+end
